ce1c1643396df0a5a5e4ba35d811d052ba4ea865
dengshuo <shuo.deng@foxmail.com>
2018-08-10 10:56:56 +0800

    动态表单

---
+++ src/component/ActiveForm.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Form, Button, Input, Select } from 'antd';
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class ActiveFormForm extends React.Component {
+  handleSubmit = () => {
+    const { getFieldsValue } = this.props.form;
+    console.log(getFieldsValue())
+
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 0 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 3 },
+      },
+    };
+
+    return <div>
+      <Form layout={this.props.layout}>
+        {this.props.obj.map((items, index) => {
+          if (items.type === 'input') {
+            return <FormItem key={index}
+              {...formItemLayout}
+            >
+              {getFieldDecorator(items.labelTxt, {
+                initialValue: items.defaultValue ? items.defaultValue : null,
+              })(
+                <Input placeholder={items.placeholder} style={{ width: 'auto' }} />
+              )}
+            </FormItem>
+          } else if (items.type === 'select') {
+            return <FormItem key={index}
+              {...formItemLayout}
+            >
+              {getFieldDecorator(items.labelTxt, {
+                initialValue: items.defaultValue ? items.defaultValue : null,
+              })(
+                <Select style={{ width: 'auto' }}>
+                  {items.opt.map((item, i) => <Option key={i} value={item.val}>{item.txt}</Option>)}
+                </Select>
+              )}
+            </FormItem>
+          }
+        })}
+        <FormItem>
+          <Button type='primary' htmlType="submit" style={{ marginLeft: '20px' }} onClick={this.handleSubmit}>查找</Button>
+        </FormItem>
+      </Form>
+
+    </div>
+  }
+}
+
+const ActiveForm = Form.create()(ActiveFormForm);
+export default ActiveForm;
--- src/container/EditPackage/index.jsx
+++ src/container/EditPackage/index.jsx
@@ -41,94 +41,100 @@ class EditPackageForm extends React.Component {
     return <div style={{ padding: '30px 0 20px' }}>
       <Row style={{ marginBottom: '20px' }}>
         <Col span={3} offset={5}>
-          <span style={{ fontWeight: 'bold' }}>安装包管理</span> > <span style={{ fontWeight: 'bold'}}>{type === 'edit' ? '编辑' : '上传安装包'}</span>
+          <span style={{ fontWeight: 'bold' }}>安装包管理</span> > <span style={{ fontWeight: 'bold' }}>{type === 'edit' ? '编辑' : '上传安装包'}</span>
         </Col>
       </Row>
-      <FormItem
-        {...formItemLayout}
-        label="安装包类型"
-      >
-        {getFieldDecorator('osType', {
-          rules: [{
-            required: true,
-          }],
-          initialValue: type === 'edit' ? 'mac' : null
-        })(
-          <Select style={{ width: '100%' }}>
-            <Option value='mac'>Mac</Option>
-            <Option value='windows'>Windows</Option>
-          </Select>
-        )}
-      </FormItem>
-      <FormItem
-        {...formItemLayout}
-        label="安装包属性"
-      >
-        {getFieldDecorator('isForceUpdate', {
-          rules: [{
-            required: true,
-          }],
-          initialValue: type === 'edit' ? 'force' : null
-        })(
-          <Select style={{ width: '100%' }}>
-            <Option value='force'>强制更新</Option>
-            <Option value='unforce'>非强制更新</Option>
-          </Select>
-        )}
-      </FormItem>
-      <FormItem
-        {...formItemLayout}
-        label="安装版本号"
-      >
-        {getFieldDecorator('versionNo', {
-          rules: [{
-            required: true,
-          }]
-        })(
-          <div>
-            <Input placeholder='请输入安装包版本号' />
-            <p style={{ marginTop: '-10px', marginBottom: '-10px' }}>同一安装包类型下的版本号不能重复</p>
+      <Form>
+        <FormItem
+          {...formItemLayout}
+          label="安装包类型"
+        >
+          {getFieldDecorator('osType', {
+            rules: [{
+              required: true,
+            }],
+            initialValue: type === 'edit' ? 'mac' : null
+          })(
+            <Select style={{ width: '100%' }}>
+              <Option value='mac'>Mac</Option>
+              <Option value='windows'>Windows</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="安装包属性"
+        >
+          {getFieldDecorator('isForceUpdate', {
+            rules: [{
+              required: true,
+            }],
+            initialValue: type === 'edit' ? 'force' : null
+          })(
+            <Select style={{ width: '100%' }}>
+              <Option value='force'>强制更新</Option>
+              <Option value='unforce'>非强制更新</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="安装版本号"
+        >
+          {getFieldDecorator('versionNo', {
+            rules: [{
+              required: true,
+            }]
+          })(
+            <div>
+              <Input placeholder='请输入安装包版本号' />
+              <p style={{ marginTop: '-10px', marginBottom: '-10px' }}>同一安装包类型下的版本号不能重复</p>
+            </div>
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="安装包特性"
+        >
+          {getFieldDecorator('description', {
+            rules: [{
+              required: true,
+            }]
+          })(
+            <div>
+              <TextArea rows={4} placeholder='请输入安装包描述' />
+            </div>
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label={type === 'edit' ? '安装包' : '添加安装包'}
+        >
+          {getFieldDecorator('installation', {
+            rules: [{
+              required: true,
+            }]
+          })(
+            <div>
+              <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                  <Icon type="inbox" />
+                </p>
+                <p className="ant-upload-text">点击或拖拽文件到此区域进行添加</p>
+                <p>（大小XXKB以内，XXX格式）</p>
+              </Dragger>
+            </div>
+          )}
+        </FormItem>
+        <FormItem>
+          <div style={{ textAlign: 'center', marginTop: '40px' }}>
+            <Button style={{ marginRight: 50 }}><Link to='/'>取消</Link></Button>
+            <Button type='primary' htmlType="submit">提交</Button>
           </div>
-        )}
-      </FormItem>
-      <FormItem
-        {...formItemLayout}
-        label="安装包特性"
-      >
-        {getFieldDecorator('description', {
-          rules: [{
-            required: true,
-          }]
-        })(
-          <div>
-            <TextArea rows={4} placeholder='请输入安装包描述' />
-          </div>
-        )}
-      </FormItem>
-      <FormItem
-        {...formItemLayout}
-        label={type === 'edit' ? '安装包' : '添加安装包'}
-      >
-        {getFieldDecorator('installation', {
-          rules: [{
-            required: true,
-          }]
-        })(
-          <div>
-            <Dragger {...props}>
-              <p className="ant-upload-drag-icon">
-                <Icon type="inbox" />
-              </p>
-              <p className="ant-upload-text">点击或拖拽文件到此区域进行添加</p>
-              <p>（大小XXKB以内，XXX格式）</p>
-            </Dragger>
-          </div>
-        )}
-      </FormItem>
-      <div style={{ textAlign: 'center', marginTop: '40px' }}>
-        <Button style={{ marginRight: 50 }}><Link to='/'>取消</Link></Button>
-        <Button type='primary'>提交</Button>
-      </div>
+        </FormItem>
+      </Form>
+
+
     </div>
   }
 }
