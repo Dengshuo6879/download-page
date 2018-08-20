08be8f1cbe85cb4e207c26596949071d73524309
dengshuo <shuo.deng@foxmail.com>
2018-08-20 17:31:11 +0800

    微调

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+document.title = 'Codecraft安装包管理台';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
